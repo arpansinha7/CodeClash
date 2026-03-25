188d7e64ec49b51e0c8c64f751e7c059b8385264
Arpan Sinha <itsarpansinha@hotmail.com>
2026-03-26 01:11:10 +0530

    Delete Client directory

--- Client/js/ui.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".create").addEventListener("click", () => {
-    localStorage.setItem("action", "create");
-    window.location.href = "lobby.html";
-});
-document.querySelector(".join").addEventListener("click", () => {
-    localStorage.setItem("action", "join");
-    window.location.href = "lobby.html";
-});
